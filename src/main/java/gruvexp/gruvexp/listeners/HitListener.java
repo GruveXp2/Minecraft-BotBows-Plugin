4812dcdf4add244a2b77513b56c30a8507cdfd33
GruveXp <gruveexp123@gmail.com>
2024-11-03 17:56:21 +0100

    fix: remove friendly fire before match

--- src/main/java/gruvexp/gruvexp/listeners/HitListener.java
+++ src/main/java/gruvexp/gruvexp/listeners/HitListener.java
@@ -13,25 +13,24 @@ public class HitListener implements Listener {
 
     @EventHandler
     public void onHit(EntityDamageByEntityEvent e) {
-        if (!BotBowsManager.activeGame) {return;}
-        if (!(e.getDamager() instanceof Arrow arrow)) {
+        if ((e.getDamager() instanceof Arrow arrow)) {
+            if (!(arrow.getShooter() instanceof Player attacker)) {return;} // den som skøyt
+            if (!(e.getEntity() instanceof Player defender)) {return;} // den som blei hitta
+            if (!BotBowsManager.isPlayerJoined(attacker) || !BotBowsManager.isPlayerJoined(defender)) {return;} // hvis de ikke er i gamet
+            arrow.setKnockbackStrength(8);
+            if (BotBowsManager.getTeam(attacker) == BotBowsManager.getTeam(defender) || attacker.isGlowing()) {
+                arrow.remove(); // if the player already was hit and has a cooldown, or if the hit player is of the same team as the attacker, the arrow won't do damage
+                e.setCancelled(true);
+                return;
+            }
+            e.setDamage(0.01); // de skal ikke daue men bli satt i spectator til runda er ferig
+            BotBowsManager.handleHit(defender, attacker);
+            new Glow(defender).runTaskTimer(Main.getPlugin(), 0L, 40L); // 2s glowing
+        } else {
             if (!(e.getEntity() instanceof Player defender)) {return;} // den som blei hitta
             if (BotBowsManager.isPlayerJoined(defender)) {
                 e.setCancelled(true); // cant damage ingame players without bow
-            }
-            return;
-        } // entitien som utførte damag
-        if (!(arrow.getShooter() instanceof Player attacker)) {return;} // den som skøyt
-        if (!(e.getEntity() instanceof Player defender)) {return;} // den som blei hitta
-        if (!BotBowsManager.isPlayerJoined(attacker) || !BotBowsManager.isPlayerJoined(defender)) {return;} // hvis de ikke er i gamet
-        arrow.setKnockbackStrength(8);
-        if (BotBowsManager.getTeam(attacker) == BotBowsManager.getTeam(defender) || attacker.isGlowing()) {
-            arrow.remove(); // if the player already was hit and has a cooldown, or if the hit player is of the same team as the attacker, the arrow won't do damage
-            e.setCancelled(true);
-            return;
+            }// entitien som utførte damag
         }
-        e.setDamage(0.01); // de skal ikke daue men bli satt i spectator til runda er ferig
-        BotBowsManager.handleHit(defender, attacker);
-        new Glow(defender).runTaskTimer(Main.getPlugin(), 0L, 40L); // 2s glowing
     }
 }
