82b0c8adcf373d6a39228263515f67e7db5a703d
GruveXp <gruveexp123@gmail.com>
2024-11-20 12:36:31 +0100

    refactor: move validation to BotBows manager

--- src/main/java/gruvexp/gruvexp/commands/StartCommand.java
+++ src/main/java/gruvexp/gruvexp/commands/StartCommand.java
@@ -1,7 +1,6 @@
 package gruvexp.gruvexp.commands;
 
 import gruvexp.gruvexp.twtClassic.BotBows;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,13 +9,7 @@ import org.bukkit.entity.Player;
 public class StartCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (BotBows.activeGame) {
-            sender.sendMessage(STR."\{ChatColor.RED}The game has already started!");
-        } else if (BotBows.team1.isEmpty() || BotBows.team2.isEmpty()) {
-            sender.sendMessage(STR."\{ChatColor.RED}Cant start game, both teams must have at least 1 player each");
-        } else {
-            BotBows.startGame((Player) sender);
-        }
+        BotBows.startGame((Player) sender);
         return true;
     }
 }
